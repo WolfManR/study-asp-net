e42e4d89accf1e4331b83dbeb74f98e3abddb05c
Tarxos <ftarxos@gmail.com>
2022-06-25 01:32:46 +0300

    make blank agent data models

---
+++ GeekBrains/MetricsManagement/MetricsManagement.Agent.Data/Metric.cs
@@ -0,0 +1,7 @@
+﻿namespace MetricsManagement.Agent.Data;
+
+public class Metric
+{
+    public int Value { get; set; }
+    public long Time { get; set; }
+}
---
+++ GeekBrains/MetricsManagement/MetricsManagement.Agent.Data/Repository.cs
@@ -0,0 +1,30 @@
+﻿namespace MetricsManagement.Agent.Data
+{
+    public abstract class Repository
+    {
+        private static List<Metric> _metrics = new();
+        public string TableName { get; set; }
+
+        public void Create(int value, long time)
+        {
+            _metrics.Add(new(){Value = value, Time = time});
+        }
+
+        public IEnumerable<Metric> Get(DateTimeOffset from, DateTimeOffset to)
+        {
+            var fromSeconds = from.ToUnixTimeSeconds();
+            var toSeconds = to.ToUnixTimeSeconds();
+
+            if (fromSeconds == toSeconds)
+            {
+                return _metrics.Where(e => e.Time == fromSeconds);
+            }
+
+            var (min, max) = fromSeconds > toSeconds
+                ? (fromSeconds, toSeconds)
+                : (toSeconds, fromSeconds);
+
+            return _metrics.Where(e=>e.Time >= min && e.Time < max);
+        }
+    }
+}
