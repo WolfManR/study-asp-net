120a85a39d4c0615a2edff9d16fb3fb78c0c9105
Tarxos <ftarxos@gmail.com>
2022-06-27 01:51:02 +0300

    add manager data project and models

---
+++ GeekBrains/MetricsManagement/Manager/MetricsManagement.Manager.Data/Agent.cs
@@ -0,0 +1,11 @@
+﻿namespace MetricsManagement.Manager.Data
+{
+    public class Agent
+    {
+        public Agent(string uri) => Uri = uri;
+
+        public int Id { get; init; }
+        public string Uri { get; }
+        public bool IsEnabled { get; set; }
+    }
+}
---
+++ GeekBrains/MetricsManagement/Manager/MetricsManagement.Manager.Data/Metric.cs
@@ -0,0 +1,9 @@
+﻿namespace MetricsManagement.Manager.Data;
+
+public class Metric
+{
+    public int Value { get; set; }
+    public long Time { get; set; }
+
+    public int AgentId { get; set; }
+}
