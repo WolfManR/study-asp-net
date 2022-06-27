af115319b07b5b0a52c523b3cd76dc97dd462ac0
Tarxos <ftarxos@gmail.com>
2022-06-28 01:44:15 +0300

    add endpoints for manager and shorten querieing agents time

--- GeekBrains/MetricsManagement/Manager/MetricsManagement.Manager/Program.cs
+++ GeekBrains/MetricsManagement/Manager/MetricsManagement.Manager/Program.cs
@@ -1,6 +1,9 @@
 using MetricsManagement.Manager.Client;
 using MetricsManagement.Manager.Data;
 using MetricsManagement.Manager.Jobs;
+
+using Microsoft.AspNetCore.Mvc;
+
 using Quartz;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -36,6 +39,36 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapPost("agent/register", ([FromBody] string uri, [FromServices] AgentsRepository agentsRepository) =>
+{
+    var id = agentsRepository.Register(uri);
+    return Results.Ok(id);
+});
+
+app.MapPost("agent/{agentId}/enable", (int agentId, [FromServices] AgentsRepository agentsRepository) =>
+{
+    agentsRepository.Enable(agentId);
+    return Results.Ok();
+});
+
+app.MapPost("agent/{agentId}/disable", (int agentId, [FromServices] AgentsRepository agentsRepository) =>
+{
+    agentsRepository.Enable(agentId);
+    return Results.Ok();
+});
+
+app.MapPost("agents", ([FromServices] AgentsRepository agentsRepository) =>
+{
+    var agents = agentsRepository.Get();
+    return Results.Ok(agents);
+});
+
+app.MapGet("metrics", (int agentId, [FromServices] MetricsRepository repository) =>
+{
+    var metrics = repository.Get(agentId, DateTimeOffset.Now.AddHours(-12), DateTimeOffset.Now);
+    return Results.Ok(metrics);
+});
+
 app.Run();
 
 
@@ -43,4 +76,4 @@ static void ConfigureJobTrigger(ITriggerConfigurator t) => t
     .StartNow()
     .WithSimpleSchedule(s => s
         .RepeatForever()
-        .WithIntervalInMinutes(4));
+        .WithIntervalInMinutes(1));
