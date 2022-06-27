cc4499e465652e2aaea0597d49fb5bab1fb9ec12
Tarxos <ftarxos@gmail.com>
2022-06-27 03:01:54 +0300

    add metrics http client

---
+++ GeekBrains/MetricsManagement/Manager/MetricsManagement.Manager/Client/MetricsClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using MetricsManagement.Manager.Data;
+
+namespace MetricsManagement.Manager.Client;
+
+public class MetricsClient
+{
+    private readonly HttpClient _client;
+
+    public MetricsClient(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<ICollection<Metric>> GetMetrics(string agentUri, string metricUri, DateTimeOffset from, DateTimeOffset to)
+    {
+        using var response = await _client.GetAsync($"{agentUri}/{metricUri}", HttpCompletionOption.ResponseHeadersRead);
+        if (!response.IsSuccessStatusCode) return Array.Empty<Metric>();
+
+        await using var stream = await response.Content.ReadAsStreamAsync();
+        var result = await JsonSerializer.DeserializeAsync<ICollection<Metric>>(stream);
+        return result ?? Array.Empty<Metric>();
+    }
+}
