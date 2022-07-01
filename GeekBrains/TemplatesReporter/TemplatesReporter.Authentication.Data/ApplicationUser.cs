b2ff51111deb4ba1d183fc795a7a5124d22e7a01
Tarxos <ftarxos@gmail.com>
2022-07-01 22:38:14 +0300

    add user identity model, db context and authentication service

---
+++ GeekBrains/TemplatesReporter/TemplatesReporter.Authentication.Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TemplatesReporter.Authentication.Data;
+
+public sealed class ApplicationUser : IdentityUser
+{
+    public string Token { get; init; }
+}
