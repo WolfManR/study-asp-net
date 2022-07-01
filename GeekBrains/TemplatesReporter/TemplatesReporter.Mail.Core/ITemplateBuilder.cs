a42f063aa0d1df8fa5afb993d056bfc6a74ed985
Tarxos <ftarxos@gmail.com>
2022-07-01 19:14:55 +0300

    add mail content builder implementation that use RazorEngineCore project

---
+++ GeekBrains/TemplatesReporter/TemplatesReporter.Mail.Core/ITemplateBuilder.cs
@@ -0,0 +1,6 @@
+﻿namespace TemplatesReporter.Mail.Core;
+
+public interface ITemplateBuilder
+{
+    Task<string> Build(string template, object model);
+}
---
+++ GeekBrains/TemplatesReporter/TemplatesReporter.Mail.RazorEngineCore/MailContentBuilder.cs
@@ -0,0 +1,24 @@
+﻿using RazorEngineCore;
+using TemplatesReporter.Mail.Core;
+
+namespace TemplatesReporter.Mail.RazorEngineCore;
+
+public class MailContentBuilder : ITemplateBuilder
+{
+    private readonly IRazorEngine _razorEngine;
+
+    public MailContentBuilder(IRazorEngine razorEngine)
+    {
+        _razorEngine = razorEngine;
+    }
+
+    public async Task<string> Build(string template, object model)
+    {
+        var templateCompiler = await _razorEngine.CompileAsync(template);
+
+        // it make errors
+        var mailContent = await templateCompiler.RunAsync(model);
+
+        return mailContent;
+    }
+}
