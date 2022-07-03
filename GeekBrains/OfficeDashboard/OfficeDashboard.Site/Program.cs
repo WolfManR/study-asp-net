1591968353d17720311c7ba21de44f9f7f0b1172
Tarxos <ftarxos@gmail.com>
2022-07-04 02:07:56 +0300

    inject data base and models generator service

--- GeekBrains/OfficeDashboard/OfficeDashboard.Site/Program.cs
+++ GeekBrains/OfficeDashboard/OfficeDashboard.Site/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using OfficeDashboard.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<OfficeDbContext>((provider, builder) =>
+{
+    var configuration = provider.GetRequiredService<IConfiguration>();
+    var connectionString = configuration.GetConnectionString("Default");
+    builder.UseNpgsql(connectionString);
+});
+builder.Services.AddTransient<OfficeRepository>();
 
 var app = builder.Build();
 
@@ -17,5 +28,14 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<OfficeDbContext>();
+    if (dbContext.Database.GetPendingMigrations().Any())
+    {
+        dbContext.Database.Migrate();
+    }
+}
 
 app.Run();
