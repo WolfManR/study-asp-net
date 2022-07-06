004fc3dc837d4e1618ae39afd976d7cf43ca9c1c
Tarxos <ftarxos@gmail.com>
2022-07-07 01:37:24 +0300

    add ef storage strategy for accounts

---
+++ GeekBrains/Bank/Accounts/Bank.Accounts.Data.EF/AccountsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bank.Accounts.Data.EF;
+
+public sealed class AccountsDbContext : DbContext
+{
+    public AccountsDbContext(DbContextOptions<AccountsDbContext> options) : base(options)
+    {
+
+    }
+
+#nullable disable
+    public DbSet<AccountEntity> Accounts { get; init; }
+#nullable restore
+}
