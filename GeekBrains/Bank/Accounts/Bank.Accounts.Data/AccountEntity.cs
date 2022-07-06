a4699f0212c0047bd7a128f7e0d7b28ce461a8ca
Tarxos <ftarxos@gmail.com>
2022-07-07 00:56:33 +0300

    simplify AccountEntity struct, because account not a card

--- GeekBrains/Bank/Accounts/Bank.Accounts.Data/AccountEntity.cs
+++ GeekBrains/Bank/Accounts/Bank.Accounts.Data/AccountEntity.cs
@@ -3,8 +3,6 @@
 public sealed class AccountEntity
 {
     public int Id { get; init; }
-    public string Number { get; set; }
+    public string IdentityId { get; set; }
     public string Holder { get; set; }
-    public int ExpireMonth { get; set; }
-    public int ExpireYear { get; set; }
 }
