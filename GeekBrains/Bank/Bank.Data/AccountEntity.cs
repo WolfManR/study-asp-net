cd860e40c6e5ff0b3fe594d09692860e736ff5a3
Tarxos <ftarxos@gmail.com>
2022-07-06 19:59:15 +0300

    add data layer and models for bank account and connection string builder

---
+++ GeekBrains/Bank/Bank.Data/AccountEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Bank.Data;
+
+public sealed class AccountEntity
+{
+    public int Id { get; init; }
+    public string Number { get; set; }
+    public string Holder { get; set; }
+    public int ExpireMonth { get; set; }
+    public int ExpireYear { get; set; }
+}
