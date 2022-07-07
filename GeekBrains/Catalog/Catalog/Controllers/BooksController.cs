0f867cb87fc682a8ec1a42c3b0bda06321611fbb
Tarxos <ftarxos@gmail.com>
2022-07-08 01:07:09 +0300

    add books catalog controller and neo4j database configuration

---
+++ GeekBrains/Catalog/Catalog/Controllers/BooksController.cs
@@ -0,0 +1,52 @@
+﻿using Catalog.Books;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers;
+
+[Route("books")]
+[ApiController]
+public class BooksController : ControllerBase
+{
+    private readonly IBooksRepository _booksRepository;
+
+    public BooksController(IBooksRepository booksRepository)
+    {
+        _booksRepository = booksRepository;
+    }
+
+    [HttpPost("store")]
+    public async Task<IActionResult> Store([FromBody] StoreBooksRequest request)
+    {
+        await _booksRepository.Store(request.Book, request.Author, request.Amount);
+        return Ok();
+    }
+
+    [HttpPost("count/{bookTitle}")]
+    public async Task<IActionResult> CountStored([FromQuery] string bookTitle)
+    {
+        var count = await _booksRepository.CountStoredBooks(bookTitle);
+        return Ok(count);
+    }
+
+    [HttpGet]
+    public async IAsyncEnumerable<BookResponse> ListStored()
+    {
+        BookResponse cache = new();
+        await foreach (var book in _booksRepository.ListBooks())
+        {
+            cache.Title = book.Title;
+            cache.Description = book.Description;
+            cache.Author = $"{book.AuthorFirstName} {book.AuthorLastName}";
+            yield return cache;
+        }
+    }
+}
+
+public record StoreBooksRequest(Book Book, Author Author, int Amount);
+public record BookResponse()
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Author { get; set; }
+}
