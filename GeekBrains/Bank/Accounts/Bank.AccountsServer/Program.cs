1dd02c4dfbbd9cc5ce38fb43e82b7a4c3dc49761
Tarxos <ftarxos@gmail.com>
2022-07-07 23:48:55 +0300

    refactor initialize code for accounts server

--- GeekBrains/Bank/Accounts/Bank.AccountsServer/Program.cs
+++ GeekBrains/Bank/Accounts/Bank.AccountsServer/Program.cs
@@ -1,6 +1,7 @@
-using App.Metrics.AspNetCore;
 using App.Metrics;
+using App.Metrics.AspNetCore;
 using App.Metrics.Formatters.Prometheus;
+
 using Bank.Accounts.Data;
 using Bank.Accounts.Data.Dapper;
 using Bank.Accounts.Data.EF;
@@ -18,80 +19,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services
-    .AddDbContext<AccountsDbContext>((p, o) =>
-    {
-        var configuration = p.GetRequiredService<IConfiguration>();
-        var connectionString = configuration.GetConnectionStringBuilder("EF").BuildWithDatabase();
-        o.UseNpgsql(connectionString);
-    })
-    .AddScoped<EFAccountsStorageStrategy>();
+RegisterDatabase(builder.Services);
 
-builder.Services
-    .AddScoped<IDbConnector, PostgresConnector>(provider =>
-    {
-        var configuration = provider.GetRequiredService<IConfiguration>();
-        var connectionString = configuration.GetConnectionStringBuilder("Dapper").BuildWithDatabase();
-        return new(connectionString);
-    })
-    .AddScoped<PostgresDbInitializer>(provider =>
-    {
-        var configuration = provider.GetRequiredService<IConfiguration>();
-        var connectionString = configuration.GetConnectionStringBuilder("Dapper");
-        return new(connectionString);
-    })
-    .AddScoped<DapperAccountsStorageStrategy>();
-
-builder.Services
-    .AddFluentMigratorCore()
-    .ConfigureRunner(c => c
-        .AddPostgres()
-        .WithGlobalConnectionString(provider =>
-        {
-            var configuration = provider.GetRequiredService<IConfiguration>();
-            var connectionString = configuration.GetConnectionStringBuilder("Dapper").BuildWithDatabase();
-            return connectionString;
-        })
-        .ScanIn(typeof(Program).Assembly).For.Migrations());
-
-builder.Services.AddScoped<AccountsRepository>();
-
-builder.Services.AddMetrics(metricsBuilder => metricsBuilder
-    .OutputMetrics.AsPrometheusPlainText()
-    .OutputMetrics.AsPrometheusProtobuf());
-
-builder.Host
-    .UseMetricsWebTracking()
-    .UseMetrics(o => o.EndpointOptions = mo =>
-    {
-        mo.MetricsTextEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter();
-        mo.MetricsEndpointOutputFormatter = new MetricsPrometheusProtobufOutputFormatter();
-        mo.EnvironmentInfoEndpointEnabled = false;
-    });
+RegisterMetrics(builder.Services, builder.Host);
 
 var app = builder.Build();
 
-await using (var scope = app.Services.CreateAsyncScope())
-{
-    try
-    {
-        await using var context = scope.ServiceProvider.GetRequiredService<AccountsDbContext>();
-        await context.Database.MigrateAsync();
-
-        var dapperContext = scope.ServiceProvider.GetRequiredService<PostgresDbInitializer>();
-        await dapperContext.InitializeAccountsDatabase();
-
-        var dapperMigration = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
-        dapperMigration.MigrateUp();
-    }
-    catch (Exception ex)
-    {
-        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-        logger.LogCritical(ex, "Fail connect to databases");
-        Environment.Exit(-1);
-        return;
-    }
-}
+if (!await TryInitializeDatabase(app.Services)) return -1;
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -103,3 +37,87 @@ if (app.Environment.IsDevelopment())
 app.MapControllers();
 
 app.Run();
+return 0;
+
+
+static void RegisterDatabase(IServiceCollection services)
+{
+    services
+        .AddDbContext<AccountsDbContext>((p, o) =>
+        {
+            var configuration = p.GetRequiredService<IConfiguration>();
+            var connectionString = configuration.GetConnectionStringBuilder("EF").BuildWithDatabase();
+            o.UseNpgsql(connectionString);
+        })
+        .AddScoped<EFAccountsStorageStrategy>();
+
+    services
+        .AddScoped<IDbConnector, PostgresConnector>(provider =>
+        {
+            var configuration = provider.GetRequiredService<IConfiguration>();
+            var connectionString = configuration.GetConnectionStringBuilder("Dapper").BuildWithDatabase();
+            return new(connectionString);
+        })
+        .AddScoped<PostgresDbInitializer>(provider =>
+        {
+            var configuration = provider.GetRequiredService<IConfiguration>();
+            var connectionString = configuration.GetConnectionStringBuilder("Dapper");
+            return new(connectionString);
+        })
+        .AddScoped<DapperAccountsStorageStrategy>();
+
+    services
+        .AddFluentMigratorCore()
+        .ConfigureRunner(c => c
+            .AddPostgres()
+            .WithGlobalConnectionString(provider =>
+            {
+                var configuration = provider.GetRequiredService<IConfiguration>();
+                var connectionString = configuration.GetConnectionStringBuilder("Dapper").BuildWithDatabase();
+                return connectionString;
+            })
+            .ScanIn(typeof(Program).Assembly).For.Migrations());
+
+    services.AddScoped<AccountsRepository>();
+}
+
+static async ValueTask<bool> TryInitializeDatabase(IServiceProvider provider)
+{
+    await using var scope = provider.CreateAsyncScope();
+    try
+    {
+        await using var context = scope.ServiceProvider.GetRequiredService<AccountsDbContext>();
+        await context.Database.MigrateAsync();
+
+        var dapperContext = scope.ServiceProvider.GetRequiredService<PostgresDbInitializer>();
+        await dapperContext.InitializeAccountsDatabase();
+
+        var dapperMigration = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+        dapperMigration.MigrateUp();
+        return true;
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogCritical(ex, "Fail connect to databases");
+        return false;
+
+    }
+}
+
+static void RegisterMetrics(IServiceCollection services, IHostBuilder host)
+{
+    services
+        .AddMetrics(metricsBuilder => metricsBuilder
+            .OutputMetrics.AsPrometheusPlainText()
+            .OutputMetrics.AsPrometheusProtobuf());
+
+    host
+        .UseMetricsWebTracking()
+        .UseMetrics(o => o.EndpointOptions = mo =>
+        {
+            mo.MetricsTextEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter();
+            mo.MetricsEndpointOutputFormatter = new MetricsPrometheusProtobufOutputFormatter();
+            mo.EnvironmentInfoEndpointEnabled = false;
+        });
+}
