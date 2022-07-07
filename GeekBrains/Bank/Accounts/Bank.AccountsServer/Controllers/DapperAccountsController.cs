85de7804a8b0804ecd14719393a3cfb9f6133729
Tarxos <ftarxos@gmail.com>
2022-07-07 03:06:22 +0300

    add blank controllers that work based on storage strategy

---
+++ GeekBrains/Bank/Accounts/Bank.AccountsServer/Controllers/DapperAccountsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.AccountsServer.Controllers;
+
+[Route("accounts/dapper")]
+[ApiController]
+public class DapperAccountsController : ControllerBase
+{
+}
---
+++ GeekBrains/Bank/Accounts/Bank.AccountsServer/Controllers/EFAccountsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.AccountsServer.Controllers;
+
+[Route("accounts/ef")]
+[ApiController]
+public class EFAccountsController : ControllerBase
+{
+}
