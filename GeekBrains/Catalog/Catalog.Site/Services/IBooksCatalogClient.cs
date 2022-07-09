95fc5f01d8a58f42c73fd8753d1d1bb4d950340f
Tarxos <ftarxos@gmail.com>
2022-07-10 01:39:48 +0300

    add books catalog refit client

---
+++ GeekBrains/Catalog/Catalog.Site/Services/IBooksCatalogClient.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Books;
+using Refit;
+
+namespace Catalog.Site.Services;
+
+public interface IBooksCatalogClient
+{
+    [Post("/books/store")]
+    Task Store([Body] StoreBooksRequest request);
+
+    [Post("books/count/{bookTitle}")]
+    Task<int> CountStored([Query] string bookTitle);
+
+    [Get("/books")]
+    // TODO: change to async enumerable when it will be released
+    Task<IEnumerable<BookResponse>> ListStored();
+}
