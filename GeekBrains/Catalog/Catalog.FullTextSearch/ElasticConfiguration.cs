3e21ffa2cfd34097cfc5be450939e6b0afe70dec
Tarxos <ftarxos@gmail.com>
2022-07-10 21:01:02 +0300

    add elastic search full text search service

---
+++ GeekBrains/Catalog/Catalog.FullTextSearch/ElasticConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Catalog.FullTextSearch;
+
+public class ElasticConfiguration
+{
+    public string Uri { get; set; }
+    public string Index { get; set; }
+}
---
+++ GeekBrains/Catalog/Catalog.FullTextSearch/ElasticService.cs
@@ -0,0 +1,44 @@
+﻿using Catalog.Books;
+
+using Microsoft.Extensions.Options;
+
+using Nest;
+
+namespace Catalog.FullTextSearch;
+
+public class ElasticService
+{
+    private readonly ElasticClient _client;
+
+    public ElasticService(IOptions<ElasticConfiguration> options)
+    {
+        var configuration = options.Value;
+        _client = new ElasticClient(BuildConnectionSetting(configuration));
+    }
+
+    public void IndexBooks(IReadOnlyCollection<BookInfo> books)
+    {
+        _client.BulkAsync(b => b.IndexMany(books));
+    }
+
+    public void IndexBook(BookInfo book)
+    {
+        _client.IndexDocument(book);
+    }
+
+    public async Task<IReadOnlyCollection<BookInfo>> Search(string title = "")
+    {
+        var result = await _client.SearchAsync<BookInfo>(d => d
+            .Query(q => q.Match(m => m.Field(b => b.Title).Query(title)))
+            .Size(100)
+        );
+        return result.Documents;
+    }
+
+    private static IConnectionSettingsValues BuildConnectionSetting(ElasticConfiguration configuration)
+    {
+        Uri uri = new(configuration.Uri);
+        return new ConnectionSettings(uri)
+            .DefaultMappingFor<BookInfo>(m => m.IndexName(configuration.Index));
+    }
+}
